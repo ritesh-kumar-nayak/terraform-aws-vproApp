4fa884c212adc45231558d28dea1ab38f897fd03
Ritesh <ritesh@gmail.com>
2023-10-05 08:06:42 +0530

    VPC

--- key-pairs.tf
+++ key-pairs.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "Vprofile-key" {
 
-    key_name = "VproProfile-Key-Terra"
-    public_key = file(var.PUBLIC_KEY_PATH) #key has been created using ssh-keygen and path has been stored in variable file
-  
+  key_name   = "VproProfile-Key-Terra"      # this will be the name of the key 
+  public_key = file(var.PUBLIC_KEY_PATH) #key has been created using ssh-keygen and path has been stored in variable file
+
 }
